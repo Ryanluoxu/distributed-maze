90b6b5d70c20a10ddaedd13188a563d79fea537a
xu.luo <xu.luo@advance.ai>
2020-08-23 00:04:56 +0800

    add Game

--- src/main/java/nus/cs5223/Game.java
+++ src/main/java/nus/cs5223/Game.java
@@ -1,23 +1,66 @@
 package nus.cs5223;
 
-import nus.cs5223.dto.GenerateServerReqDTO;
-import nus.cs5223.dto.GenerateServerResDTO;
-import nus.cs5223.dto.JoinGameReqDTO;
-import nus.cs5223.dto.JoinGameResDTO;
+import nus.cs5223.remote.TrackerRemote;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Game {
 
+    private static final String REMOTE_REF_TRACKER = "tracker";
+    private static String host = "127.0.0.1";
+    private static int port = 1099;
+    private static String playerId;
+
     public static void main(String[] args) {
-        /**
-         * java Game [IP-address] [port-number] [player-id]
-         *  - IP-address is the well-known IP address of the Tracker
-         *  - port-number is the port over which the Tracker is listening
-         *  - player-id is the two-character name of the player
-         */
+        readArgs(args);
+
+        TrackerRemote trackerStub = getTrackerStub();
 
 
+    }
+
+    private static TrackerRemote getTrackerStub() {
+        TrackerRemote trackerStub = null;
+        try {
+            Registry registry = LocateRegistry.getRegistry(host, port);
+            trackerStub = (TrackerRemote) registry.lookup(REMOTE_REF_TRACKER);
+        } catch (RemoteException | NotBoundException ex) {
+            System.err.println("readArgs error: " + ex.getMessage());
+            System.exit(0);
+        }
+        return trackerStub;
+    }
+
+    /**
+     * java Game [IP-address] [port-number] [player-id]
+     * - IP-address is the well-known IP address of the Tracker
+     * - port-number is the port over which the Tracker is listening
+     * - player-id is the two-character name of the player
+     */
+    private static void readArgs(String[] args) {
+        if (args != null && args.length == 3) {
+            try {
+                host = args[0];
+                port = Integer.parseInt(args[1]);
+                playerId = args[2];
+                if (playerId.length() != 2) {
+                    exitGame(args);
+                }
+            } catch (Exception ex) {
+                System.err.println("readArgs error: " + ex.getMessage());
+                exitGame(args);
+            }
+        } else {
+            exitGame(args);
+        }
+    }
+
+    private static void exitGame(String[] args) {
+        System.err.println("invalid args: " + args);
+        System.exit(0);
     }
 
 }
--- src/main/java/nus/cs5223/Tracker.java
+++ src/main/java/nus/cs5223/Tracker.java
@@ -7,6 +7,9 @@ import nus.cs5223.dto.JoinGameResDTO;
 import nus.cs5223.remote.TrackerRemote;
 
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
 public class Tracker implements TrackerRemote {
 
@@ -14,15 +17,27 @@ public class Tracker implements TrackerRemote {
      * The rmi-registry should be used only for registering and locating the tracker.
      */
 
+    private static final String REMOTE_REF = "tracker";
+    private static int port = 0;
+    private static int N = 10;
+    private static int K = 15;
+
+
     public static void main(String[] args) {
-        /**
-         * java Tracker [port-number] [N] [K]
-         *  - port-number is the port over which the Tracker is listening
-         *  - The (implicit) IP address will be the local machine’s IP
-         */
+        readArgs(args);
 
+        try {
+            Tracker tracker = new Tracker();
+            TrackerRemote stub = (TrackerRemote) UnicastRemoteObject.exportObject(tracker, port);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind(REMOTE_REF, stub);
 
+            System.out.println("tracker ready... remote ref: " + REMOTE_REF + " port: " + port + ", N: " + N + ", K: " + K + ".");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
+
 
     @Override
     public JoinGameResDTO joinGame(JoinGameReqDTO request) throws RemoteException {
@@ -32,5 +47,28 @@ public class Tracker implements TrackerRemote {
     @Override
     public GenerateServerResDTO generateServer(GenerateServerReqDTO request) throws RemoteException {
         return null;
+    }
+
+    /**
+     * java Tracker [port-number] [N] [K]
+     * - port-number is the port over which the Tracker is listening
+     * - The (implicit) IP address will be the local machine’s IP
+     */
+    private static void readArgs(String[] args) {
+        if (args != null && args.length == 3) {
+            try {
+                port = Integer.parseInt(args[0]);
+                N = Integer.parseInt(args[1]);
+                K = Integer.parseInt(args[2]);
+            } catch (Exception ex) {
+                System.err.println("readArgs error: " + ex.getMessage());
+                exitGame(args);
+            }
+        }
+    }
+
+    private static void exitGame(String[] args) {
+        System.err.println("invalid args: " + args);
+        System.exit(0);
     }
 }
