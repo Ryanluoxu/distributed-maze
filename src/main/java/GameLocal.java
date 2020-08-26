ff2d248a9b07ea37cdec0e62934c62df2fea8f8b
xu.luo <xu.luo@advance.ai>
2020-08-26 13:52:53 +0800

    GameLocal and GameRemote

--- src/main/java/GameLocal.java
+++ src/main/java/GameLocal.java
@@ -2,5 +2,8 @@ import java.util.List;
 
 public interface GameLocal {
 
+    /**
+     * 首位玩家 tracker.getGameInfo 之后：initGame 得到 gameState
+     */
     GameStateVO initGame(int N, int K, List<PlayerVO> playerList);
 }
