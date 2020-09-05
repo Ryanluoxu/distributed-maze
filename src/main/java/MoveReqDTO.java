76745270b4c935d8758551e86e01918e399bf9a2
Yu Jiahan <116010274@link.cuhk.edu.cn>
2020-09-05 19:08:17 +0800

    update move request

--- src/main/java/MoveReqDTO.java
+++ src/main/java/MoveReqDTO.java
@@ -1,2 +1,28 @@
+import java.util.HashMap;
 public class MoveReqDTO {
+    private String playerId;
+    private Integer move;
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public Integer getMove() {
+        return move;
+    }
+
+    public String toString() {
+        HashMap<Integer, String> moveDir = new HashMap<Integer, String>();
+        moveDir.put(0,"Unmoved");
+        moveDir.put(1,"West");
+        moveDir.put(2,"South");
+        moveDir.put(3,"East");
+        moveDir.put(4,"North");
+        moveDir.put(9,"Quit");
+
+        return "MoveReqDTO{" +
+                "playerId='" + playerId +
+                ", move=" + moveDir.get(move) +
+                '}';
+    }
 }
